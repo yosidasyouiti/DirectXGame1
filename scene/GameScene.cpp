08b2c40a49bd5ea6cd643df21c5e6b1b445106cb
yosidasyouiti <k021g1106@g.neec.ac.jp>
2022-04-19 13:05:54 +0900

    追加

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,11 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +18,42 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	//３Dモデル生成
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	//サウンドデータ読み込み
+	soundDateHandle_ = audio_->LoadWave("se_sad03.wav");
+	//音声再生
+	audio_->PlayWave(soundDateHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDateHandle_, true);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+	XMFLOAT2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+	//スペースで音が止まる
+	if (input_->TriggerKey(DIK_SPACE)){
+		audio_->StopWave(voiceHandle_);
+	}
+	//デバックテキスト
+	debugText_->Print("kaizokuou ni oreha naru", 50, 50, 1.0f);
+	//書式指定
+	debugText_->SetPos(50,70);
+	debugText_->Printf("year:%d", 2001);
+	//値加算
+	value_++;
+	//値の文字列
+	std::string strDebug = std::string("value") +
+		std::to_string(value_);
+	//テキスト表示
+	debugText_->Print(strDebug, 50, 150, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -40,11 +77,10 @@ void GameScene::Draw() {
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(commandList);
-
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -56,12 +92,12 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-
+	sprite_->Draw();
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
 	// スプライト描画後処理
 	Sprite::PostDraw();
-
+	
 #pragma endregion
 }
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -51,4 +51,17 @@ class GameScene {
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	///テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	//3Dモデル
+	Model* model_ = nullptr;
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+	//サウンドデータハンドル
+	uint32_t soundDateHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+	//値表示
+	int32_t value_ = 0;
 };
